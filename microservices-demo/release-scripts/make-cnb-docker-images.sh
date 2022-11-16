2f736705b0947f87e2cbe418bb84427248ccb2d9
Bogdan Pastiu <v-bpastiu@digitalocean.com>
2022-11-16 14:26:00 +0200

    Adds comments in the script

--- microservices-demo/release-scripts/make-cnb-docker-images.sh
+++ microservices-demo/release-scripts/make-cnb-docker-images.sh
@@ -21,6 +21,7 @@ while IFS= read -d $'\0' -r dir; do
     (
         cd "${builddir}"
         log "Building and pushing: ${image}"
+        ## entrypoint needs to be specifically set when using buildpacks with python projects 
         if [ $svcname == "emailservice" ]
         then
             pack build ${image} --env "GOOGLE_ENTRYPOINT=python email_server.py" --builder gcr.io/buildpacks/builder:v1
@@ -29,10 +30,12 @@ while IFS= read -d $'\0' -r dir; do
         then
             pack build ${image} --env "GOOGLE_ENTRYPOINT=recommendationservice/recommendation_server.py" --builder gcr.io/buildpacks/builder:v1
         fi
+        ## skipping loadgenerator service building as it's used only in CI for tests
         if [ $svcname == "loadgenerator" ]
         then 
             continue
         fi
+        ## we are not using the --publish pack flag due to an issue preventing packer from pushing to DOCR
         pack build ${image} --builder gcr.io/buildpacks/builder:v1
         docker push ${image}
     )
