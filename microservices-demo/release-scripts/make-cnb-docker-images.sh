862b418d64189fc2eb58b2431dd6ab5efa5f43bf
Bogdan Pastiu <v-bpastiu@digitalocean.com>
2022-11-16 10:03:11 +0200

    Adds script to create images using cloud native buildpacks

---
+++ microservices-demo/release-scripts/make-cnb-docker-images.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+log() { echo "$1" >&2; }
+
+TAG="${TAG:?TAG env variable must be specified}"
+REPO_PREFIX="${REPO_PREFIX:?REPO_PREFIX env variable must be specified}"
+
+while IFS= read -d $'\0' -r dir; do
+    # build image
+    svcname="$(basename "${dir}")"
+    builddir="${dir}"
+    #PR 516 moved cartservice build artifacts one level down to src
+    if [ $svcname == "cartservice" ] 
+    then
+        builddir="${dir}/src"
+    fi
+    image="${REPO_PREFIX}/$svcname:$TAG"
+    (
+        cd "${builddir}"
+        log "Building and pushing: ${image}"
+        if [ $svcname == "emailservice" ]
+        then
+            pack build ${image} --env "GOOGLE_ENTRYPOINT=python email_server.py" --builder gcr.io/buildpacks/builder:v1
+        fi
+        if [ $svcname == "recommendationservice" ]
+        then
+            pack build ${image} --env "GOOGLE_ENTRYPOINT=recommendationservice/recommendation_server.py" --builder gcr.io/buildpacks/builder:v1
+        fi
+        pack build ${image} --builder gcr.io/buildpacks/builder:v1
+        docker push ${image}
+    )
+done < <(find "${SCRIPTDIR}/../src" -mindepth 1 -maxdepth 1 -type d -print0)
+
+log "Successfully built and pushed all images."
