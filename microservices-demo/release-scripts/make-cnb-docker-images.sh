1074cb30254993b8d8c81c5ffd6407940bd83f29
Bogdan Pastiu <v-bpastiu@digitalocean.com>
2022-11-17 13:29:06 +0200

    Adds procfiles with entrypoints

--- microservices-demo/release-scripts/make-cnb-docker-images.sh
+++ microservices-demo/release-scripts/make-cnb-docker-images.sh
@@ -17,27 +17,18 @@ while IFS= read -d $'\0' -r dir; do
     then
         builddir="${dir}/src"
     fi
+    ## skipping loadgenerator service building as it's used only in CI for tests
+    if [ $svcname == "loadgenerator" ]
+    then 
+        continue
+    fi
     image="${REPO_PREFIX}/$svcname:$TAG"
     (
         cd "${builddir}"
         log "Building and pushing: ${image}"
-        ## entrypoint needs to be specifically set when using buildpacks with python projects 
-        if [ $svcname == "emailservice" ]
-        then
-            pack build ${image} --env "GOOGLE_ENTRYPOINT=python email_server.py" --builder gcr.io/buildpacks/builder:v1
-        fi
-        if [ $svcname == "recommendationservice" ]
-        then
-            pack build ${image} --env "GOOGLE_ENTRYPOINT=recommendationservice/recommendation_server.py" --builder gcr.io/buildpacks/builder:v1
-        fi
-        ## skipping loadgenerator service building as it's used only in CI for tests
-        if [ $svcname == "loadgenerator" ]
-        then 
-            continue
-        fi
-        ## we are not using the --publish pack flag due to an issue preventing packer from pushing to DOCR
+        ## we are not using the --publish pack flag due to an issue preventing the pack CLI from pushing to DOCR
         pack build ${image} --builder gcr.io/buildpacks/builder:v1
-        docker push ${image}
+        # docker push ${image}
     )
 done < <(find "${SCRIPTDIR}/../src" -mindepth 1 -maxdepth 1 -type d -print0)
 
